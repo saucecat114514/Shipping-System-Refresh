a3cecb16a6d83f07d7a3a529f6045e02129499d3
LinYongxin <14886601+moldy-foam@user.noreply.gitee.com>
2025-06-13 22:49:35 +0800

    添加前端模块

---
+++ web/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+}
+</style>
